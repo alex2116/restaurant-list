78799a50c7ced4e6570bcf56160dd93deccacdea
alex2116 <reingeld116@gmail.com>
2021-07-15 16:04:23 +0800

    phone input type=text, restaurant edit function use Object.assign

--- app.js
+++ app.js
@@ -53,9 +53,8 @@ app.get('/detail/:restaurant_id', (req, res) => {
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
-  console.log(keyword)
-  const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().replace(/\s+/g, '').includes(keyword.toLowerCase().replace(/\s+/g, '')))
-  res.render('index', {restaurantList: restaurants, keyword: keyword})
+  const restaurants = Restaurant.filter(restaurants => restaurants.name.toLowerCase().replace(/\s+/g, '').includes(keyword.toLowerCase.replace(/\s+/g, '')))
+  return res.render('index', {restaurant: restaurants})
 })
 
 app.post('/add-restaurant', (req, res) => {
@@ -76,29 +75,12 @@ app.post('/add-restaurant', (req, res) => {
 
 app.post('/:restaurant_id/edit', (req, res) => {
   const id = req.params.restaurant_id
-  const name = req.body.name
-  const name_en = req.body.name_en
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const google_map = req.body.google_map
-  const rating = req.body.rating
-  const description = req.body.description
 
   return Restaurant.findById(id)
     .then(restaurant => {
-      restaurant.name = name
-      restaurant.name_en = name_en
-      restaurant.category = category
-      restaurant.image = image
-      restaurant.location = location
-      restaurant.phone = phone
-      restaurant.google_map = google_map
-      restaurant.rating = rating
-      restaurant.description = description
+      restaurant = Object.assign(restaurant, req.body)
       return restaurant.save()
-     })
+    })
     .then(() => res.redirect(`/detail/${id}`))
     .catch(error => console.log(error))
 })
