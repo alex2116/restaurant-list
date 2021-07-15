25d3c1a26d0ac9ed61f2d3a789309631ce7fc73e
alex2116 <reingeld116@gmail.com>
2021-07-15 13:49:09 +0800

    add ceate page & edit page

--- app.js
+++ app.js
@@ -34,24 +34,13 @@ app.get('/restarant/new', (req,res) => {
   return res.render('new')
 })
 
-app.get('/restaurant-detail/:restaurant_id', (req,res) => {
-
-})
-
-app.post('/add-restaurant', (req,res) => {
-  const name = req.body.name
-  const name_en = req.body.name_en
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const google_map = req.body.google_map
-  const rating = req.body.rating
-  const description = req.body.description  
+app.get('/edit/:restaurant_id', (req,res) => {
+  const id = req.params.restaurant_id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', {restaurant}))
+    .catch(error => console.log)
   
-  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
 })
 
 app.get('/detail/:restaurant_id', (req, res) => {
@@ -67,6 +56,51 @@ app.get('/search', (req, res) => {
   console.log(keyword)
   const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().replace(/\s+/g, '').includes(keyword.toLowerCase().replace(/\s+/g, '')))
   res.render('index', {restaurantList: restaurants, keyword: keyword})
+})
+
+app.post('/add-restaurant', (req, res) => {
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+app.post('/:restaurant_id/edit', (req, res) => {
+  const id = req.params.restaurant_id
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = google_map
+      restaurant.rating = rating
+      restaurant.description = description
+      return restaurant.save()
+     })
+    .then(() => res.redirect(`/detail/${id}`))
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
