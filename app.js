c6d826ad1caa164b2da0320405909ce9b6fe837e
alex2116 <reingeld116@gmail.com>
2021-08-05 15:40:21 +0800

    feat: add express-session & dotenv

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 require('./config/mongoose')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const app = express()
 const port = 3000
@@ -10,6 +14,11 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true}))
 app.use(methodOverride('_method'))
