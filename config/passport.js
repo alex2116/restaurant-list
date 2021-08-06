af87235e8ed2afc17246c3bb9a403baa0148ed02
alex2116 <reingeld116@gmail.com>
2021-08-06 13:55:15 +0800

    feat: add bcrypt to login route

--- config/passport.js
+++ config/passport.js
@@ -1,24 +1,29 @@
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
 module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
-  passport.use(new LocalStrategy({ 
-    usernameField: 'email',
-    passReqToCallback: true}, (req, email, password, done) => {
-    User.findOne({ email })
-    .then(user => {
-      if (!user) {
-        return done(null, false, req.flash('failureFlash', 'That email is not registered!'))
-      }
-      if (user.password !== password) {
-        return done(null, false, req.flash('failureFlash', 'Email or Password incorrect.'))
-      }
-      return done(null, user)      
-    })
-    .catch(err => done(err, false))
-  }))
+
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true
+    }, (req, email, password, done) => {
+      User.findOne({ email })
+        .then(user => {
+          if (!user) {
+            return done(null, false, req.flash('failureFlash', 'That email is not registered!'))
+          }
+          return bcrypt.compare(password, user.password)
+            .then(isMatch => {
+              if (!isMatch) {
+                return done(null, false, req.flash('failureFlash', 'Email or Password incorrect.'))
+              }
+          return done(null, user)   
+          })
+        })
+        .catch(err => done(err, false))
+    }))
+
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
